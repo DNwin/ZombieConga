921099697fbdf5809eee404c0dca49c54d74b490
DNwin <ndennis@gmail.com>
2015-06-05 03:45:49 -0700

    Initial Commit

---
+++ ZombieConga/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZombieConga
+//
+//  Created by Dennis Nguyen on 6/5/15.
+//  Copyright (c) 2015 dnwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZombieConga/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  ZombieConga
+//
+
+//  Copyright (c) 2015 dnwin. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
