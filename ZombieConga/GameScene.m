67d30c7624be2fc7f498c4789bb0ff98c74480be
DNwin <ndennis@gmail.com>
2015-06-06 03:03:55 -0700

    CG Math Utility and Operator Functions

--- ZombieConga/GameScene.m
+++ ZombieConga/GameScene.m
@@ -7,6 +7,7 @@
 //
 
 #import "GameScene.h"
+#import "MyUtils.h"
 
 @interface GameScene()
 
---
+++ ZombieConga/MyUtils.h
@@ -0,0 +1,68 @@
+//
+//  MyUtils.h
+//  ZombieConga
+//
+//  Created by Dennis Nguyen on 6/6/15.
+//  Copyright (c) 2015 dnwin. All rights reserved.
+//
+
+#ifndef ZombieConga_MyUtils_h
+#define ZombieConga_MyUtils_h
+
+#import <Foundation/Foundation.h>
+
+static inline CGPoint CGPointAdd(const CGPoint a,
+                                 const CGPoint b)
+{
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+
+static inline CGPoint CGPointSubtract(const CGPoint a,
+                                      const CGPoint b)
+{
+    return CGPointMake(a.x - b.x, a.y - b.y);
+}
+
+static inline CGPoint CGPointMultiplyScalar(const CGPoint a,
+                                            const CGFloat b)
+{
+    return CGPointMake(a.x * b, a.y * b);
+}
+
+//Hypotenuse
+static inline CGFloat CGPointLength(const CGPoint a)
+{
+    return sqrtf(a.x * a.x + a.y * a.y);
+}
+
+//Unit vector
+static inline CGPoint CGPointNormalize(const CGPoint a)
+{
+    CGFloat length = CGPointLength(a);
+    return CGPointMake(a.x / length, a.y / length);
+}
+
+static inline CGFloat CGPointToAngle(const CGPoint a)
+{
+    return atan2f(a.y, a.x);
+}
+
+static inline CGFloat ScalarSign(CGFloat a)
+{
+    return a >= 0 ? 1 : -1;
+}
+
+// Returns shortest angle between two angles,
+// between -M_PI and M_PI
+static inline CGFloat ScalarShortestAngleBetween(
+                                                 const CGFloat a, const CGFloat b)
+{
+    CGFloat difference = b - a;
+    CGFloat angle = fmodf(difference, M_PI * 2);
+    if (angle >= M_PI) {
+        angle -= M_PI * 2;
+    }
+    return angle;
+}
+
+#endif
